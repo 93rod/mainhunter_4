f314d2ca5ebbea2aeb781a2115180a05f610f358
93rod <135771500+93rod@users.noreply.github.com>
2023-06-28 17:03:21 +0200

    conf_file.py

--- conf_file.py
+++ conf_file.py
@@ -3,17 +3,15 @@ import time
 
 def do():
     # Connexion à la base de données
-    conn = sqlite3.connect('/home/lica/rootkit/hashes')
+    conn = sqlite3.connect('~/rootkit/hashes')
     cursor = conn.cursor()
 
     # Lecture du contenu du fichier conf
-    with open('/home/lica/rootkit/rkh-conf', 'r') as file:
+    with open('~/rootkit/rkh-conf', 'r') as file:
         lines = file.readlines()
         for line in lines:
             time.sleep(3)
-            # Ajout de chaque ligne dans la colonne "path" de la base de données
             cursor.execute("INSERT INTO myhashes (path) VALUES (?)", (line.strip(),))
             conn.commit()
         
-    # Fermeture de la connexion à la base de données
     conn.close()
